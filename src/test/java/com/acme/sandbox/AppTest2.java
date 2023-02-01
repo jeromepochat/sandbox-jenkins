ac7a9a6bafaaa482cecb549e707544e302190154
Jérôme Pochat <jpochat@cloudbees.com>
2023-02-01 11:03:14 +0100

    Update

---
+++ src/test/java/com/acme/sandbox/AppTest2.java
@@ -0,0 +1,19 @@
+package com.acme.sandbox;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest2 {
+
+    @Test
+    public void testFast() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testSlow() {
+        assertTrue(true);
+    }
+}
